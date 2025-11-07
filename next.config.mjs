078d306148425b0a019f0ae503705b4702561245
DumarMalpica <dumarmalpica@outlook.es>
2025-11-07 15:35:41 -0500

    changes to deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone",
+  env: {
+    PORT: process.env.PORT || 8080,
+  },
+  experimental: {
+    serverActions: true
+  }
+};
 
 export default nextConfig;
