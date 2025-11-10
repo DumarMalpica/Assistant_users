af5f30a76342b5334bd0435a2ab9b5c0e4664379
DumarMalpica <dumarmalpica@outlook.es>
2025-11-10 12:16:59 -0500

    elimination of unnecessary functions

--- app/components/file-viewer.tsx
+++ app/components/file-viewer.tsx
@@ -62,7 +62,7 @@ const FileViewer = () => {
         }`}
       >
         {files.length === 0 ? (
-          <div className={styles.title}>Attach files to test file search</div>
+          <div className={styles.title}>Adjuntar archivos para probar la búsqueda de archivos</div>
         ) : (
           files.map((file) => (
             <div key={file.file_id} className={styles.fileEntry}>
@@ -79,7 +79,7 @@ const FileViewer = () => {
       </div>
       <div className={styles.fileUploadContainer}>
         <label htmlFor="file-upload" className={styles.fileUploadBtn}>
-          Attach files
+          Subir Archivo
         </label>
         <input
           type="file"
